319f2d5b50ef14ae7ce420cedc8fd0fbfac3290b
Jaskaran Singh <jaskaran1523@gmail.com>
2021-12-10 16:10:34 -0800

    Create image slider

--- pages/hotel/[id].js
+++ pages/hotel/[id].js
@@ -1,12 +1,28 @@
+import SimpleImageSlider from "react-simple-image-slider";
 import { Box } from "@chakra-ui/layout";
 
 import Navbar from "../../components/Navbar";
 import { baseUrl, getData } from "../../utils/api";
 
 const HotelDetails = ({ hotelDetails, hotelPhotos }) => {
+	const images = hotelPhotos.hotelImages.slice(0, 10).map((image) => ({
+		url: image.baseUrl.replace("{size}", "z"),
+	}));
+
+	console.log(images);
+
 	return (
 		<Box>
 			<Navbar />
+			<Box m="25px auto 0 auto" w="896px">
+				<SimpleImageSlider
+					width={896}
+					height={504}
+					images={images}
+					showBullets={true}
+					showNavs={true}
+				/>
+			</Box>
 		</Box>
 	);
 };
