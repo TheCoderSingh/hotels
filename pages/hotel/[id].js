839f495d6a859db28006d9f2011a27a3260c3873
Jaskaran Singh <jaskaran1523@gmail.com>
2021-12-10 22:38:06 -0800

    Add hotel details

--- pages/hotel/[id].js
+++ pages/hotel/[id].js
@@ -1,20 +1,24 @@
 import SimpleImageSlider from "react-simple-image-slider";
-import { Box } from "@chakra-ui/layout";
+import Image from "next/image";
+import { Box, Heading, Text } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/react";
 
 import Navbar from "../../components/Navbar";
 import { baseUrl, getData } from "../../utils/api";
+import LocationImage from "../../assets/location.png";
 
 const HotelDetails = ({ hotelDetails, hotelPhotos }) => {
 	const images = hotelPhotos.hotelImages.slice(0, 10).map((image) => ({
 		url: image.baseUrl.replace("{size}", "z"),
 	}));
 
-	console.log(images);
-
 	return (
 		<Box>
 			<Navbar />
 			<Box m="25px auto 0 auto" w="896px">
+				<Heading as="h3" mb={3}>
+					{hotelDetails.data.body.propertyDescription.name}
+				</Heading>
 				<SimpleImageSlider
 					width={896}
 					height={504}
@@ -22,6 +26,69 @@ const HotelDetails = ({ hotelDetails, hotelPhotos }) => {
 					showBullets={true}
 					showNavs={true}
 				/>
+				<Box cursor="pointer">
+					<a
+						href={
+							hotelDetails.data.body.propertyDescription.mapWidget
+								.staticMapUrl
+						}
+						target="_blank"
+					>
+						<Flex alignItems="center" mt={3}>
+							<Image
+								src={LocationImage}
+								width="30px"
+								height="28px"
+							/>
+							<Text color="#777" fontSize="0.8em">
+								{
+									hotelDetails.data.body.propertyDescription
+										.address.fullAddress
+								}
+							</Text>
+						</Flex>
+					</a>
+				</Box>
+				<Box mt={4}>
+					<Flex wrap="wrap" justifyContent="space-between">
+						{hotelDetails.data.body.overview.overviewSections.map(
+							(amenity) => (
+								<Box>
+									<Heading fontSize="18px">
+										{amenity.title}
+									</Heading>
+									<Text>
+										{amenity.content.map((amenity) => (
+											<Text>- {amenity}</Text>
+										))}
+									</Text>
+								</Box>
+							)
+						)}
+					</Flex>
+				</Box>
+				<Box mt={5}>
+					<Heading fontSize="24px" mb={1}>
+						Hotel Amenities
+					</Heading>
+					<hr />
+					<Flex justifyContent="space-between">
+						{hotelDetails.data.body.amenities[0].listItems.map(
+							(amenity) => (
+								<Box mt={3}>
+									<Heading fontSize={18}>
+										{amenity.heading}
+									</Heading>
+									{amenity.listItems.map((amenity) => (
+										<Text fontSize="0.8em">
+											- {amenity}
+										</Text>
+									))}
+								</Box>
+							)
+						)}
+					</Flex>
+				</Box>
 			</Box>
 		</Box>
 	);
