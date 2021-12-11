b1ab1386d88d579b0c728e01abd9f8ef4f049163
Jaskaran Singh <jaskaran1523@gmail.com>
2021-12-10 22:44:41 -0800

    Add loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import Router from "next/router";
+import nProgress from "nprogress";
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/styles.css";
 
 function MyApp({ Component, pageProps }) {
+	nProgress.configure({ showSpinner: false });
+
+	Router.events.on("routeChangeStart", () => {
+		nProgress.start();
+	});
+
+	Router.events.on("routeChangeComplete", () => {
+		nProgress.done();
+	});
+
 	return (
 		<ChakraProvider>
 			<Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,13 @@ export default function Home({ hotels }) {
 					content="Search rooms, prices and more from many hotels around the world!"
 				/>
 				<link rel="icon" href="/favicon.ico" />
+				<link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+					integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+					crossOrigin="anonymous"
+					referrerPolicy="no-referrer"
+				/>
 			</Head>
 
 			<Box>
